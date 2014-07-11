a4309308dbe0db7154ad9aeadf40b10811fdaa7c
Xanderhed <xanderhed@gmail.com>
2014-07-11 12:00:14 -0600

    My Spritz Clone v0.2

--- src/source/speedSelector.java
+++ src/source/speedSelector.java
@@ -1,20 +1,42 @@
 package source;
 
 import java.awt.BorderLayout;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class speedSelector extends JPanel {
+@SuppressWarnings("serial")
+public class speedSelector extends JPanel implements KeyListener{
 
+	private static final String[] speeds = {"50", "100", "150", "200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "700", "750", "800", "850", "900", "950", "1000"};
+	
+	@SuppressWarnings("unused")
 	private JLabel label;
-	private JComboBox speed;
+	private JComboBox<String> comboBox;
 	
 	public speedSelector() {
 		
 		super(new BorderLayout());
-		String[] speeds = {"100", "200", "300", "400", "500", "600", "700", "800", "900", "1000"};
 		this.add(label = new JLabel("Read Speed"), BorderLayout.NORTH);
-		this.add(speed = new JComboBox(speeds), BorderLayout.SOUTH);
+		this.add(comboBox = new JComboBox<>(speeds), BorderLayout.SOUTH);
+		this.addKeyListener(this);
+	}
+	
+	public int getSpeed() {
+		
+		int selectedSpeed = Integer.parseInt(speeds[comboBox.getSelectedIndex()]);
+		return selectedSpeed;
+	}
+
+	public void keyPressed(KeyEvent arg0) {
+	}
+
+	public void keyReleased(KeyEvent arg0) {
+	}
+
+	public void keyTyped(KeyEvent arg0) {
 	}
 }
