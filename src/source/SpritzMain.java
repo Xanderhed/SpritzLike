dc86cffcfa6b56249bb353c1604b541c46328cf9
Xanderhed <xanderhed@gmail.com>
2014-07-18 18:41:28 -0600

    My Spritz Clone v0.5
    The spritzing itself now functions, the beginnings of the key bindings
    are in place but non functional.

--- src/source/SpritzMain.java
+++ src/source/SpritzMain.java
@@ -1,26 +1,146 @@
 package source;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.KeyStroke;
 
 
-public class SpritzMain{
+@SuppressWarnings("serial")
+public class SpritzMain extends Container implements Runnable{
+	
+	public static final String NAME = "Alex's Spritz Clone";
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 300;
+	
+	public SpritzPanel panel;
+	public JLabel text;
+	
+	private boolean running;
+	private JFrame frame;
+	
+	public SpritzMain () {
+		
+		frame = new JFrame(NAME);
+		frame.setLayout(new BorderLayout());
+		frame.add(this, BorderLayout.CENTER);
+		
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.setMinimumSize(new Dimension(WIDTH / 2, HEIGHT / 2));
+		this.setMaximumSize(new Dimension(WIDTH * 2, HEIGHT * 2));
+		frame.pack();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+	public void start() {
+		
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public void stop() {
+		
+		running = false;
+	}
+	
+	public void init() {
+		
+		this.setLayout(new BorderLayout());
+		this.add(panel = new SpritzPanel(), BorderLayout.NORTH);
+		this.add(text = new JLabel("Text appears here."), BorderLayout.CENTER);
+		text.setHorizontalAlignment(JLabel.CENTER);
+		text.setFont(text.getFont().deriveFont(48f));
+		bind();
+		this.revalidate();
+		this.setVisible(true);
+	}
+	
+	public void bind() {
+		
+		pauseAction pause = new pauseAction();
+		//nextAction next = new nextAction();
+		//lastAction last = new lastAction();
+		
+		panel.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0), "doPause");
+		//panel.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "doNext");
+		//panel.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "doLast");
+		
+		panel.getActionMap().put("doPause", pause);
+		//panel.getActionMap().put("doNext", next);
+		//panel.getActionMap().put("doLast", last);
+	}
+	
+	public void run() {
+		
+		init();
+		long lastTime = System.currentTimeMillis();
+		long delta = 0;
+		
+		while(running) {
+			if(!panel.fileLoaded) {
+				System.out.println("No file has been selected");
+				continue;
+			}
+			int speed = panel.getSpeed();
+			long currentTime = System.currentTimeMillis();
+			Double timeBetweenTicks = 60000.0 / (double)speed;
+			delta += currentTime - lastTime;
+			lastTime = currentTime;
+			if(delta >= timeBetweenTicks) {
+				delta = 0;
+				tickAndRender();
+			}
+		}
+	}
+	
+	private void tickAndRender() {
+		System.out.println("The system has ticked");
+		try {
+			text.setText(panel.words.get(panel.getIndex()));
+			if(!panel.paused) {
+				panel.nextWord();
+			}
+			text.revalidate();
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			panel.paused = true;
+			panel.lastWord();
+		}
+	}
 	
 	public static void main(String[] args) {
 		
-		SwingUtilities.invokeLater(new Runnable() {
-			
-			public void run() {
-				
-				final JFrame frame = new JFrame("Alex's Spritz Clone");
-				final Container container = frame.getContentPane();
-				frame.setSize(300, 200);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				container.add(new SpritzPanel());
-				frame.setVisible(true);
-			}
-		});
+		new SpritzMain().start();
 	}
-
+	
+	private class pauseAction extends AbstractAction {
+		
+		public void actionPerformed(ActionEvent spacebar) {
+			System.out.println("The spacebar has been registered");
+			panel.paused = !panel.paused;
+		}
+	}
+	
+	private class nextAction extends AbstractAction {
+		
+		public void actionPerformed(ActionEvent right) {
+			
+		}
+	}
+	
+	private class lastAction extends AbstractAction {
+		
+		public void actionPerformed(ActionEvent left) {
+			
+		}
+	}
 }
--- src/source/SpritzPanel.java
+++ src/source/SpritzPanel.java
@@ -1,27 +1,45 @@
 package source;
 
-import java.io.*;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.filechooser.*;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class SpritzPanel extends JPanel implements ActionListener {
+public class SpritzPanel extends JPanel implements ActionListener{
 
 	private JButton openFileButton;
 	private speedSelector speedSelect;
 	private JFileChooser fileSelect;
+	private File file;
+	private int index;
+	
+	
+	public ArrayList<String> words;
+	
+	public boolean fileLoaded;
+	public boolean paused;
 	
 	//constructor
 	public SpritzPanel() {
 		
 		this.add(openFileButton = new JButton("Open File"));
 		this.add(speedSelect = new speedSelector());
+		fileLoaded = false;
+		paused = false;
+		index = 0;
+		words = new ArrayList<String>();
 		fileSelect = new JFileChooser();
+		openFileButton.setFocusable(false);
+		speedSelect.setFocusable(false);
 		openFileButton.addActionListener(this);
+		//this.addKeyListener(this);
 	}
 	
 	//interface methods
@@ -32,9 +50,41 @@ public class SpritzPanel extends JPanel implements ActionListener {
 			int returnValue = fileSelect.showOpenDialog(SpritzPanel.this);
 			if(returnValue == JFileChooser.APPROVE_OPTION) {
 				
-				File file = fileSelect.getSelectedFile();
-				//TODO pass the file to a worker thread somehow
+				file = fileSelect.getSelectedFile();
+				parseFile();
+				fileLoaded = true;
 			}
 		}
+	}
+	
+	public int getSpeed() {
+		
+		return speedSelect.getSpeed();
+	}
+	
+	private void parseFile() {
+		words.clear();
+		try {
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNext()) {
+				words.add(scanner.next());
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void nextWord() {
+		index++;
+	}
+	
+	public void lastWord() {
+		index--;
+	}
+	
+	public int getIndex() {
+		return index;
 	}
 }
